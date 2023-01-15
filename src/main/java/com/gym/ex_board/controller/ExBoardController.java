0191abaab7898957d5deb341e9e3478df36e798d
Cheol0007 <syuri0007@gmail.com>
2023-01-15 18:17:20 +0900

    게시판 글 작성 테스트버전 업로드

--- src/main/java/com/gym/ex_board/controller/ExBoardController.java
+++ src/main/java/com/gym/ex_board/controller/ExBoardController.java
@@ -1,6 +1,16 @@
 package com.gym.ex_board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping(value="/ex_board")
 public class ExBoardController {
 
+	   @RequestMapping(value="/ex_test", method=RequestMethod.GET)
+	   public void Ex_Test() {
+	      
+	   }
+	   
 }
